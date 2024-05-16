d9769628682c0c7716c1455845aab2a454bb4ebc
sitapawar <sita_pawar@brown.edu>
2024-05-16 15:47:33 -0400

    better styling

--- orlando-furioso-website/src/components/CharacterNode.js
+++ orlando-furioso-website/src/components/CharacterNode.js
@@ -1,18 +1,9 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import './CharacterNode.css';
 
 const CharacterNode = ({ character, onClick, getPosition }) => {
-  const nodeRef = useRef(null);
-
-  useEffect(() => {
-    const node = nodeRef.current;
-    if (node) {
-      const { x, y } = node.getBoundingClientRect();
-      getPosition(character, x, y);
-    }
-  }, [character, getPosition]);
-
-  const handleMouseDown = () => {
+  const handleMouseDown = (event) => {
+    getPosition(character, event.clientX, event.clientY);
     onClick(character);
   };
 
@@ -28,6 +19,37 @@ const CharacterNode = ({ character, onClick, getPosition }) => {
 };
 
 export default CharacterNode;
+
+// // import React, { useEffect, useRef } from 'react';
+// // import './CharacterNode.css';
+
+// // const CharacterNode = ({ character, onClick, getPosition }) => {
+// //   const nodeRef = useRef(null);
+
+// //   useEffect(() => {
+// //     const node = nodeRef.current;
+// //     if (node) {
+// //       const { x, y } = node.getBoundingClientRect();
+// //       getPosition(character, x, y);
+// //     }
+// //   }, [character, getPosition]);
+
+// //   const handleMouseDown = () => {
+// //     onClick(character);
+// //   };
+
+// //   // Determine the CSS class based on the character's army
+// //   const armyClass = character.army === "Charlemagne's Army" ? "charlemagne-army" : 
+// //                    character.army === "Agramante's Army" ? "agramante-army" : "";
+
+// //   return (
+// //     <div className={`character-node ${armyClass}`} onMouseDown={handleMouseDown}>
+// //       <p>{character.name}</p>
+// //     </div>
+// //   );
+// // };
+
+// // export default CharacterNode;
 
 
 // import React from 'react';
